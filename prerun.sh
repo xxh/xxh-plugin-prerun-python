5bd22eee0c543270250fde557c9e03eaa0f7bc67
anki-code <anki-code>
2020-05-02 19:43:18 +0300

    xonsh

--- prerun.sh
+++ prerun.sh
@@ -1,5 +1,7 @@
-CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PYTHONHOME=`dirname $XDG_CONFIG_HOME`/.local
-export PYTHONUSERBASE=$PYTHONHOME
-export PYTHONPATH=$PYTHONHOME/lib/python3.8/site-packages:$PYTHONPATH
-export PATH=$PYTHONHOME/bin:$CDIR/python:$PATH
+if [[ $XXH_SHELL != "xonsh" ]]; then
+  CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  export PYTHONHOME=`dirname $XDG_CONFIG_HOME`/.local
+  export PYTHONUSERBASE=$PYTHONHOME
+  export PYTHONPATH=$PYTHONHOME/lib/python3.8/site-packages:$PYTHONPATH
+  export PATH=$PYTHONHOME/bin:$CDIR/python:$PATH
+fi
